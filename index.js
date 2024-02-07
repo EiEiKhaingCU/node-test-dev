dd1f31ccc124274d3a2023217ef3520b89508d88
Ei Ei Khaing <eieikhaing@Eis-MacBook-Pro.local>
2024-02-08 01:21:44 +0630

    index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const http = require('http')
 const app = express()
 const server = http.createServer(app)
-const PORT = 3000//process.env.PORT
+const PORT = 80//process.env.PORT
 
 const startServer = async () => {
     app.get('/', (req, res) => {
