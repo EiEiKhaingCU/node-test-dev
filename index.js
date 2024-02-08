769eeb28f47f5cd4b91c5a2fad6c81904906761e
Ei Ei Khaing <eieikhaing@Eis-MacBook-Pro.local>
2024-02-08 07:55:34 +0630

    index

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-var http = require('http');
+// 
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('A Monk in Cloud'); //write a response to the client
-  res.end(); //end the response
-}).listen(3000); //the server object listens on port 80
+
+require('dotenv').config()
+const express = require('express')
+const http = require('http')
+const app = express()
+const server = http.createServer(app)
+const PORT = 3000//process.env.PORT
+
+const startServer = async () => {
+    app.get('/', (req, res) => {
+        res.send('Hello, world! This is the index page.');
+    })
+    app.get('/api/data', (req, res) => {
+        const data = {
+          message: 'This is some data from the API route.',
+          timestamp: new Date().toISOString()
+        };
+        res.json(data);
+      });
+
+    server.listen(PORT, () => {
+        console.log(`User service is running on port : ${PORT}`)
+    })
+        .on('error', (err) => {
+            console.log(err);
+            process.exit();
+        })
+        .on('close', () => {
+            channel.close();
+        })
+
+}
+
+startServer().then(() => console.log(`Node Test App is triggering.`))
